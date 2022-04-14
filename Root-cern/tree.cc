efc8767942d496d95375e99ec5e1da802db473d5
Abdoelabassi <abderrezaq@outlook.com>
2022-04-14 11:29:42 +0000

    storing data in tree

--- Root-cern/tree.cc
+++ Root-cern/tree.cc
@@ -38,33 +38,6 @@ void tree(){
 	//Read into the tree:
 
 
-	TCanvas *c = new TCanvas();
-
-	TFile *input = new TFile("data.root", "read");
-
-	TTree *tre = (TTree*)input->Get("tree");
-
-	double a,b,ax,by;
-
-    //let's read only two vars
-	tre->SetBranchAddress("x", &a);
-	tre->SetBranchAddress("y", &b);
-
-
-	int entries = tre->GetEntries();
-
-	cout << entries << endl;
-
-	for (int i=0; i < entries; i++){
-
-		tre->GetEntry(i);
-
-		cout << a  << " " << b << endl;
-
-	}
-
-
-	input->Close();
 
 
 }
